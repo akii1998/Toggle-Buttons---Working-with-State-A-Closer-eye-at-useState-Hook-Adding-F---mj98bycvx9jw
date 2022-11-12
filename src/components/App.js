e851cb26f3f09b3597faefab7a2d88b5ad8b52e0
akii1998 <110331686+akii1998@users.noreply.github.com>
2022-11-12 18:02:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,31 @@
-import React, {useState} from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-//code here 
-  
+  //code here
+  const [ok, setok] = useState("OFF");
+  const [No, setNo] = useState("ON");
+  const handleClick = (e) => {
+    if (e.target.id === "btn1") {
+      setok("NO");
+      setNo("OFF");
+    } else {
+      setok("OFF");
+      setNo("ON");
+    }
+  };
 
-  
   return (
     <div className="App">
-      <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
+      <button id="btn1" onClick={(e) => handleClick(e)}>
+        {ok}
       </button>
       <br />
       <br />
-      <button id="btn2" onClick={handleClick}>
-        {/* assign value for button 2 */}
+      <button id="btn2" onClick={(e) => handleClick(e)}>
+        {No}
       </button>
     </div>
   );
-}
-
+};
 
 export default App;
